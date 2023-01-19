a00fd02078aa973380d23e9e2d6986a59c2c19e1
whereareyo <104694707+wherear3y0@users.noreply.github.com>
2023-01-19 14:24:26 +0300

    просто тестовое задание
    пока что одно из начальных - не понимаю до конца что мне делать и где учиться
    
    нужно разобраться

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+//одно из заданий в учебе - пока что не понимаю что мне учить и куда двигаться
+
 int main() {
-      cout << "Hello, World!" << endl;
-    return 0;
+      int x, y;
+      cin >> x >> y;
+
+      if(x >= 0 && y >= 0 && x <= y){
+          for(int z = x; z <= y; z++){
+              cout << x << endl;
+              ++x;
+          }
+      }
 }
